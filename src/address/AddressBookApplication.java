dbca8225f1a56630a6ba1049b184880762929405
George <cortes.george93@icloud.com>
2024-02-25 13:34:36 -0800

    Added displayMenu()

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -7,12 +7,12 @@ import address.data.AddressEntry;
 public class AddressBookApplication {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        AddressBook ab = new AddressBook();
-        AddressEntry entry = new AddressEntry("george", "cortes", "20222 mission blvd", "hayward", "ca", "5122220905", "chihuahuha@mail.com", 94541);
-        ab.add(entry);
-        AddressEntry entry2 = new AddressEntry("Lorem", "ipsum", "20009 yuna Rd", "Castro Valley" , "CA", "51039993999", "stuff@gmail.com",94554);
-        ab.add(entry2);
-        ab.list();
+//        AddressBook ab = new AddressBook();
+//        AddressEntry entry = new AddressEntry("george", "cortes", "20222 mission blvd", "hayward", "ca", "5122220905", "chihuahuha@mail.com", 94541);
+//        ab.add(entry);
+//        AddressEntry entry2 = new AddressEntry("Lorem", "ipsum", "20009 yuna Rd", "Castro Valley" , "CA", "51039993999", "stuff@gmail.com",94554);
+//        ab.add(entry2);
+//        ab.list();
 
     }
 
--- src/address/Menu.java
+++ src/address/Menu.java
@@ -1,8 +1,34 @@
 package address;
 
+import java.util.Scanner;
+
 public class Menu {
 
 
+    Menu(){
+        displayMenu();
+    }
+
+    public void displayMenu(){
+        char option = 0;
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.println("************************");
+            System.out.println("Please enter your menu selection");
+            System.out.println("a) Loading From File");
+            System.out.println("b) Add a Contact");
+            System.out.println("c) Remove a Contact");
+            System.out.println("d) Find a Contact");
+            System.out.println("e) Listing");
+            System.out.println("f) Quit Program");
+            System.out.println("************************");
+            option = scan.next().charAt(0);
+            if(option < 'a' || option >'f'){
+                System.out.println("Wrong input value, please try again");
+            }
+        }while(option < 'a' || option > 'f');
+        scan.close();
+    }
     /**
      * prompt for First Name
      *
